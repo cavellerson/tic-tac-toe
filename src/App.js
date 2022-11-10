ad638d248150e98529c4ffe8f8c35aab07c91f0c
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2022-11-10 06:08:41 -0500

    gameboard created and its rows

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-
+import { useState, useEffect } from 'react'
 
 function App() {
+
+    const [gameboard, setGameboard] = useState([[0,0,0],[0,0,0],[0,0,0]])
+
+    useEffect(() => {
+
+    },[])
   return (
     <div className="App">
+        <div className="gameboard">
+            <div className="row1">
+                {gameboard[0].map((box) => {
+                    return <div>{box}</div>
+                })}
+            </div>
 
+            <div className="row2">
+            {gameboard[1].map((box) => {
+                return <div>{box}</div>
+            })}
+            </div>
+
+            <div className="row3">
+            {gameboard[2].map((box) => {
+                return <div>{box}</div>
+            })}
+            </div>
+
+        </div>
     </div>
   );
 }
